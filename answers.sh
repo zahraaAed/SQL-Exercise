a6e8d1ef9c1325c85120703f531a13c2a0e8e778
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-09-18 19:05:03 +0300

    creating-tables

--- answers.sh
+++ answers.sh
@@ -17,4 +17,40 @@ WHERE Name = 'Basma';
 UPDATE Students
 SET Points = Points - 20
 WHERE Name = 'Alex';
-#answer8
+#create-table
+CREATE TABLE graduates (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation date
+);
+
+INSERT INTO graduates (Name, Age, Gender, Points, Graduation-date)
+SELECT Name, Age, Gender, Points, '08/09/2018'
+FROM students
+WHERE Name = 'Layal';
+DELETE * from students where name='Layal';
+#join
+
+SELECT Companies.Name, Companies.Date,employees.Name
+FROM Companies
+INNER JOIN employees.employees.name ON Companies.ID=employees.ID;
+SELECT Name  from employees where date from companies <2000;
+SELECT Name  from employees where role='Graphic Designer';
+
+SELECT employees.Name, Companies.Name, Companies.Date
+FROM employees
+INNER JOIN Companies
+ON employees.CompanyID = Companies.ID;
+SELECT employees.Name
+FROM employees
+INNER JOIN Companies
+ON employees.CompanyID = Companies.ID
+WHERE Companies.Date < 2000;
+SELECT Companies.Name
+FROM Companies
+INNER JOIN employees
+ON Companies.ID = employees.CompanyID
+WHERE employees.Role = 'Graphic Designer';
